2052427783b114240a8bb203acfdcf5283629fba
geandro <geandro_a@hotmail.com>
2024-04-11 19:42:18 -0300

    atualizado 0.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import Categorias from './pages/categorias/Categorias'
+
 import Clientes from './pages/cliente/Clientes'
 
 import Produtos from './pages/produtos/Produtos'
 import Home from './pages/home/Home'
+import Categorias from './pages/categorias/Categorias'
 
 function App() {
 
@@ -14,12 +14,13 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Categorias />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/cliente" element={<Clientes />} />
+        <Route path="/categoria" element={<Categorias />} />
         <Route path="/produto" element={<Produtos />} />
       </Routes>
   </BrowserRouter>
   )
 }
 
-export default App
+export default App;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,62 @@
-function Home(){
-return(
-    <>
-    <h1>
-        Igor Car
-    </h1>
+import { Link, useNavigate } from "react-router-dom";
 
-    
-    </>
-)
+function Home() {
+    let navigate = useNavigate()
+    return (
+        <>
+            <h1 className="flex justify-center text-3xl mt-5">
+                Igor Car
+            </h1>
+
+            <div className="grid grid-cols-2 gap-1">
+                <div className="flex justify-center ms-4 me-4 mt-10">
+                    <div className=" bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+
+                        <h5 className="flex justify-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Serviços</h5>
+
+                        <a href="#">
+                            <img className="rounded-t-lg" src="https://i.imgur.com/M4vtpQn.jpg" alt="" width='100%'/>
+                        </a>
+                        <div className="p-5">
+
+                            <div className="flex justify-center mt-6 ">
+                            <Link to='/cliente' className='hover:underline'>
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+                                    Entrar
+                                </button>
+
+                            </Link>
+                              
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="flex justify-center ms-4 me-4 mt-10">
+                    <div className=" bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+
+                        <h5 className="flex justify-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Estoque</h5>
+
+                        <a href="#">
+                            <img className="rounded-t-lg" src="https://i.imgur.com/AvhreaA.jpg/image-1.jpg" alt="" width='100%' />
+                        </a>
+                        <div className="p-5">
+
+                            <div className="flex justify-center  mt-6">
+                            <Link to='/categoria' className='hover:underline'>
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+                                    Entrar
+                                </button>
+
+                            </Link>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
 }
 export default Home;
