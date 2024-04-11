c9f3d720456eb347aab16613123e390b0b43503e
geandro <geandro_a@hotmail.com>
2024-04-11 19:03:50 -0300

    atualizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/home/Home'
+
 import Categorias from './pages/categorias/Categorias'
 import Clientes from './pages/cliente/Clientes'
-import Produto from './componentes/form/Pruduto'
+
 import Produtos from './pages/produtos/Produtos'
+import Home from './pages/home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,12 @@
+function Home(){
+return(
+    <>
+    <h1>
+        Igor Car
+    </h1>
+
+    
+    </>
+)
+}
+export default Home;
