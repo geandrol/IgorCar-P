4ab0fc9ffc3f284a3ec64730f54f81606cc6c73f
geandro <geandro_a@hotmail.com>
2024-04-11 20:15:31 -0300

    atualizado 0.2

--- src/pages/estoque/Estoque.tsx
+++ src/pages/estoque/Estoque.tsx
@@ -7,14 +7,14 @@ function Estoque() {
 
                 <Link to='/listarProduto' className='hover:underline'>
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
-                        Entrar
+                        Produtos
                     </button>
 
                 </Link>
 
                 <Link to='/listarCategoria' className='hover:underline'>
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
-                        Entrar
+                       Categorias
                     </button>
 
                 </Link>
